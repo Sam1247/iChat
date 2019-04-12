bcf448282ee1b8500f749b093b0af77a32a99156
saman <abdallaelsaman67@gmail.com>
2019-04-12 22:49:11 +0200

    fetching users data load in table view new messages

--- iChat/Controllers/NewMessageController.swift
+++ iChat/Controllers/NewMessageController.swift
@@ -7,15 +7,62 @@
 //
 
 import UIKit
+import Firebase
 
 class NewMessageController: UITableViewController {
+    
+    var users = [User]()
+    let cellId = "cellId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
+        tableView.register(UserCell.self, forCellReuseIdentifier: cellId)
+        fetchUser()
+    }
+    
+    func fetchUser() {
+        Database.database().reference().child("users").observe(.childAdded, with: {
+            [weak self] (snapshot) in
+            if let dictionary = snapshot.value as? [String: AnyObject] {
+                let user = User()
+                user.name = dictionary["name"] as? String
+                user.email = dictionary["email"] as? String
+                //print(user.name, user.email)
+                DispatchQueue.main.async {[weak self] in
+                    self?.tableView.reloadData()
+                }
+                self?.users.append(user)
+            }
+        }, withCancel: nil)
     }
     
     @objc func handleCancel () {
         dismiss(animated: true, completion: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        cell.textLabel?.text = users[indexPath.row].name
+        cell.detailTextLabel?.text = users[indexPath.row].email
+        
+        
+        return cell
+    }
+}
+
+class UserCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
---
+++ iChat/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  iChat
+//
+//  Created by Abdalla Elsaman on 4/12/19.
+//  Copyright © 2019 Dumbies. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var name: String?
+    var email: String?
+}
